3419d72e233f4f55959880051df319282259d945
diamondburned <diamond@arikawa-hi.me>
2023-09-19 21:36:33 -0700

    Use new code boilerplate
    See https://github.com/diamondburned/discord-workshop-env.

---
+++ bot.py
@@ -0,0 +1,169 @@
+import game
+import discord
+from discord.ext import commands
+from pyopentdb import OpenTDBClient, Category, QuestionType, Difficulty
+from utils import game_not_found_embed, get_question_embed
+
+
+bot = commands.Bot(command_prefix="/", intents=discord.Intents.all())
+
+
+@bot.event
+async def on_ready():
+    # Explain what a bot.tree is, why do we need to do that?
+    # Syncs all the slash commands into one big tree
+    await bot.tree.sync()
+
+    embed = discord.Embed(
+        title="Trivia Game Started!",
+        color=discord.Color.brand_green(),
+        description="Join using **/join** to play!",
+    )
+
+    channel = bot.get_channel(1109275176807432202)
+
+    await channel.send(embed=embed)
+
+
+# ------------------------------
+# Baseline code (blueprint for slash commands first before logic)
+# ------------------------------
+
+
+@bot.tree.command(name="join")
+async def join(interaction: discord.Interaction):
+    game_state = game.create_game(interaction.channel_id)
+
+    # game.is_running is determined by modifying to True in /start
+    if interaction.user.name not in game_state.scores:
+        # Create new user with their score if 0
+        game_state.scores[interaction.user.name] = 0
+        embed = discord.Embed(
+            title=f"{interaction.user.name}, you have joined!",
+            color=discord.Color.blue(),
+            description=f"**{len(game_state.scores)}** players waiting in line... \nIf **all players** are ready, please type **/start** to begin.",
+        )
+    else:
+        # User has already joined!
+        embed = discord.Embed(
+            title=f"{interaction.user.name}, you have already joined!",
+            color=discord.Color.red(),
+            description=f"**{len(game_state.scores)}** players waiting in line... \nIf **all players** are ready, please type **/start** to begin.",
+        )
+
+    await interaction.response.send_message(embed=embed)
+
+
+# ------------------------------
+# Here explain argument types and how to make them optional!
+# ------------------------------
+
+
+@bot.tree.command(name="start")
+@discord.app_commands.describe(amount="Amount of questions (DEFAULT - 5)")
+@discord.app_commands.choices(
+    difficulty=[
+        discord.app_commands.Choice(name="Easy", value=Difficulty.EASY.name),
+        discord.app_commands.Choice(name="Medium", value=Difficulty.MEDIUM.name),
+        discord.app_commands.Choice(name="Hard", value=Difficulty.HARD.name),
+    ]
+)
+@discord.app_commands.choices(
+    category=[
+        discord.app_commands.Choice(
+            name="General Knowledge", value=Category.GENERAL_KNOWLEDGE.name
+        ),
+        discord.app_commands.Choice(
+            name="Computer Science", value=Category.SCIENCE_COMPUTERS.name
+        ),
+    ]
+)
+async def start(
+    interaction: discord.Interaction,
+    # Make sure to use discord.app_commands.Range to ensure minimum one question
+    amount: discord.app_commands.Range[int, 1, 100] = 5,
+    # Discuss placing default values in param types
+    difficulty: str = Difficulty.EASY.name,
+    # Explain to look at the documentation
+    category: str = Category.SCIENCE_COMPUTERS.name,
+):
+    game_state = game.must_get_game(interaction.channel_id)
+
+    # If you start a game in another channel without joining first
+    if game_state is None:
+        await interaction.response.send_message(embed=game_not_found_embed())
+        return
+
+    # In case we call /start twice, we don't want to start the game twice!
+    if game_state.is_running:
+        embed = discord.Embed(
+            title=f"{interaction.user.name}, the game has already started...",
+            color=discord.Color.red(),
+        )
+        await interaction.response.send_message(embed=embed)
+        return
+
+    # Enable is_running to True! We've started the game!!!
+    game_state.is_running = True
+
+    client = OpenTDBClient()
+    questions = client.get_questions(
+        amount=int(amount),
+        category=Category[category],
+        difficulty=Difficulty[difficulty],
+        question_type=QuestionType.TRUE_FALSE,
+    )
+
+    """
+    [question='The Sun rises from the North.', choices=['False', 'True'],
+    answer='False', answer_index=0) ......., 
+    """
+
+    game_state.questions = [
+        (game.Question(question.question, question.choices, question.answer))
+        for question in questions
+    ]
+
+    # game.questions = [("QUESTION 1", ["True", "False"], "ANSWER1"), ("QUESTION 2", ["True", "False"], "ANSWER2")...]
+
+    embed = get_question_embed(interaction, game_state)
+    await interaction.response.send_message(embed=embed)
+
+
+@bot.tree.command(name="answer")
+@discord.app_commands.choices(
+    answer=[
+        discord.app_commands.Choice(name="T", value="True"),
+        discord.app_commands.Choice(name="F", value="False"),
+    ]
+)
+async def answer(
+    interaction: discord.Interaction,
+    answer: discord.app_commands.Choice[str],
+):
+    game_state = game.must_get_game(interaction.channel_id)
+    if game_state is None:
+        await interaction.response.send_message(embed=game_not_found_embed())
+        return
+
+    embeds = []  # we're sending multiple embeds
+
+    if answer.value == game_state.get_current_question().answer:
+        game_state.current_q_index += 1
+
+        game_state.scores[interaction.user.name] += 1
+        embeds = [
+            discord.Embed(
+                title=f"**{interaction.user.name} GOT THE QUESTION!**",
+                color=discord.Color.green(),
+            ),
+            # correct answer embed
+            get_question_embed(interaction, game_state),
+        ]
+    else:
+        embeds = [discord.Embed(title="**NOPE!**", color=discord.Color.red())]
+
+    await interaction.response.send_message(
+        content=f"<@{interaction.user.id}>",
+        embeds=embeds,
+    )
--- utils.py
+++ utils.py
@@ -1,60 +1,8 @@
 import os
 from typing import List, Dict
 import discord
+import game
 from dataclasses import dataclass
-
-
-# Using a dataclass basically generates constructor along
-# with others (constructor == __init__)
-@dataclass
-class Question:
-    question: str
-    choices: List[str]
-    answer: str
-
-
-class GameState:
-    current_q_index = 0
-
-    # Hold the players username as the key and the score as the value
-    scores: Dict[str, int] = {}
-    # Holds the question, choices, answer
-    questions: List[Question] = []
-    # If game is running
-    is_running: bool = False
-
-    def get_current_question(self) -> Question:
-        return self.questions[self.current_q_index]
-
-    def is_ended(self) -> bool:
-        return len(self.questions) > 0 and self.current_q_index >= len(self.questions)
-
-
-# Track one game state per channel
-game_channels: dict[int, GameState] = {}
-
-
-def must_get_game(
-    interaction: discord.Interaction,
-    create=False,
-) -> GameState | None:
-    assert interaction.channel_id is not None  # make discord.py happy :)
-    # A safety check to ensure we have a valid channel ID before doing anything with it in our code
-
-    game_state = game_channels.get(interaction.channel_id)
-    # If there is a channel that doesnt have a game running
-    if game_state is None:
-        # If we want to create a new game, create a new game state with the channel id
-        if create:
-            game_state = GameState()
-            game_channels[interaction.channel_id] = game_state
-        else:
-            return None
-
-    if game_state.is_ended():
-        return None
-
-    return game_state
 
 
 # cool little embed for when game is not found
@@ -65,23 +13,9 @@ def game_not_found_embed():
     )
 
 
-def get_token() -> str:
-    TOKEN = os.getenv("DISCORD_TOKEN")
-    if TOKEN is None:
-        raise EnvironmentError("'DISCORD_TOKEN' is not set!")
-
-    return TOKEN
-
-
-def return_sorted_leaderboard_msg(players: Dict[str, int]):
+def return_sorted_leaderboard_msg(game_state: game.GameState) -> discord.Embed:
     message_description = ""
-
-    # Briefly explain sorting dictionaries!
-    # A lambda function is a concise way to create small, anonymous functions in Python.
-    sorted_players = sorted(players.items(), key=lambda x: x[1], reverse=True)
-    # Reverse = True -> High to low
-
-    for player, score in sorted_players:
+    for player, score in game_state.leaderboard():
         message_description += f"{player} - {score}\n"
 
     return discord.Embed(
@@ -94,14 +28,13 @@ def return_sorted_leaderboard_msg(players: Dict[str, int]):
 # change the game state to the next question and return the response embed
 def get_question_embed(
     interaction: discord.Interaction,
-    game_state: GameState,
+    game_state: game.GameState,
 ) -> discord.Embed:
     # Do this at the end (SIDE - CASE)
     if game_state.is_ended():
-        return return_sorted_leaderboard_msg(game_state.scores)
+        return return_sorted_leaderboard_msg(game_state)
 
     current_question = game_state.get_current_question()
-
     formatted_choices = ", ".join(current_question.choices)
     # What it will look like - True, False
 
